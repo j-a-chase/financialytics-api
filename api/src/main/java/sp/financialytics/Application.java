fec98d08bd684f956d4654d6c17c8a7a85ade137
James Chase <gochases@gmail.com>
2025-02-06 16:45:11 -0700

    Add basic Spring Application to run

--- api/src/main/java/sp/financialytics/Application.java
+++ api/src/main/java/sp/financialytics/Application.java
@@ -1,8 +1,11 @@
 package sp.financialytics;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
